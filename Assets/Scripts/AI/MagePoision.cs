fddbb668d001b00f52e2d19d2ee2333d38242074
Tuandat <>
2024-12-04 01:07:40 +0700

    Xong quái, cập nhật chết cho nhân vật,

---
+++ Assets/Scripts/AI/MagePoision.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagePoision : MonoBehaviour
+{
+    public int dame;
+    private float time=1;
+    public bool inside = false;
+    public PlayerInfor player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = GameObject.FindWithTag("Player").GetComponent<PlayerInfor>();
+            dame = GameObject.FindWithTag("MageEnemy").GetComponent<EnemyInfor>().dame / 3;
+            inside = true;
+            StartCoroutine(ApplyDamge());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inside = false;
+        player = null;
+    }
+    private IEnumerator ApplyDamge()
+    {
+        while(inside && player != null)
+        {
+            Debug.Log("Trung doc");
+            yield return new WaitForSeconds(time);
+            player.PlayerTakeStandanDame(dame);
+        }
+    }
+}
