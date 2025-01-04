7c29c32fc7eaa02fef63ae8418fb535ce4e5d737
lelongtoan <83998616+lelongtoan@users.noreply.github.com>
2025-01-04 15:06:03 +0700

    aaa

--- Assets/Scripts/ToanLe/SettingP.cs
+++ Assets/Scripts/ToanLe/SettingP.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public class SettingP : MonoBehaviour
 {
 
-    [SerializeField] public GameObject soundPanel;
     [SerializeField] public GameObject saveGame;
     private void OnEnable()
     {
@@ -18,10 +17,6 @@ public class SettingP : MonoBehaviour
         {
             saveGame.SetActive(false);
         }
-    }
-    public void SetSoundPanel()
-    {
-        soundPanel.SetActive(!soundPanel.activeInHierarchy);
     }
     public void SaveGame()
     {
