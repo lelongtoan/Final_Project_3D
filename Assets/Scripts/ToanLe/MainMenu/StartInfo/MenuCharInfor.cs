18d03a45432fae077b7f72039ff8c4ee074913bd
lelongtoan <83998616+lelongtoan@users.noreply.github.com>
2025-01-12 18:55:18 +0700

    Revert "Toan le"

--- Assets/Scripts/ToanLe/MainMenu/StartInfo/MenuCharInfor.cs
+++ Assets/Scripts/ToanLe/MainMenu/StartInfo/MenuCharInfor.cs
@@ -16,10 +16,6 @@ public class MenuCharInfor : MonoBehaviour
     {
         Set();
     }
-    private void Update()
-    {
-        Set();
-    }
     public void Set()
     {
         statsData.Set();
